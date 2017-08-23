8ab9b7bbc7f67f3205bb2e618b9030dc376a6d96
Vijay Kumar <vijay925.phs@gmail.com>
2017-08-23 14:45:45 -0400

    implemented filter

--- mine/js/map.js
+++ mine/js/map.js
@@ -1,6 +1,138 @@
 var map;
 var markers = [];
 
+var locations = [
+    {title: 'Park Ave Penthouse', location: {lat: 40.7713024, lng: -73.9632393}},
+    {title: 'Chelsea Loft', location: {lat: 40.7444883, lng: -73.9949465}},
+    {title: 'Union Square Open Floor Plan', location: {lat: 40.7347062, lng: -73.9895759}},
+    {title: 'East Village Hip Studio', location: {lat: 40.7281777, lng: -73.984377}},
+    {title: 'TriBeCa Artsy Bachelor Pad', location: {lat: 40.7195264, lng: -74.0089934}},
+    {title: 'Chinatown Homey Space', location: {lat: 40.7180628, lng: -73.9961237}}
+];
+
+var stringStartsWith = function (string, startsWith) {
+    string = string || "";
+    if (startsWith.length > string.length)
+        return false;
+    return string.substring(0, startsWith.length) === startsWith;
+};
+
+function AppViewModel() {
+  var self = this;
+
+  self.searchString = ko.observable('');
+  self.locationsVisible = ko.observableArray();
+
+  self.init = function() {
+    for(var i = 0; i < locations.length; i++) {
+      self.locationsVisible.push(locations[i]);
+    }
+  }
+
+  self.filter = function() {
+    var lowerCaseSearchString = self.searchString().toLowerCase();
+
+    if(!lowerCaseSearchString) {
+      self.locationsVisible.removeAll();
+      self.init();
+      self.createAndShowMarkers();
+    }
+    else
+    {
+      self.locationsVisible.removeAll();
+      ko.utils.arrayFilter(locations, function(item) {
+        if(stringStartsWith(item.title.toLowerCase(), lowerCaseSearchString))
+          self.locationsVisible.push(item);
+      });
+      self.createAndShowMarkers();
+    }
+  };
+
+  self.createAndShowMarkers = function() {
+    markers.length = 0;
+
+    var defaultIcon = makeMarkerIcon('0091ff');
+    var highlightedIcon = makeMarkerIcon('FFFF24');
+
+    for(var i = 0; i < self.locationsVisible().length; i++) {
+      var position = self.locationsVisible()[i].location;
+      var title = self.locationsVisible()[i].title;
+
+    var marker = new google.maps.Marker({
+      position: position,
+      title: title,
+      animation: google.maps.Animation.DROP,
+      icon: defaultIcon,
+      id: i
+    });
+
+    markers.push(marker);
+    marker.setMap(map);
+
+    marker.addListener('click', function() {
+      populateInfoWindow(this, largeInfowindow);
+    });
+
+    marker.addListener('mouseover', function() {
+      this.setIcon(highlightedIcon);
+    });
+    marker.addListener('mouseout', function() {
+      this.setIcon(defaultIcon);
+    });
+
+  } //for
+  };
+
+}
+
+var VM = new AppViewModel();
+ko.applyBindings(VM);
+VM.init();
+
+VM.searchString.subscribe(function() {
+    VM.filter();
+});
+
+/*
+function createAndShowMarkers() {
+  //console.log("updateMarkers called");
+  markers.length = 0;
+
+  var defaultIcon = makeMarkerIcon('0091ff');
+  var highlightedIcon = makeMarkerIcon('FFFF24');
+
+  for(var i = 0; i < locationsVisible().length; i++) {
+    var position = locationsVisible()[i].location;
+    var title = locationsVisible()[i].title;
+
+    var marker = new google.maps.Marker({
+      position: position,
+      title: title,
+      animation: google.maps.Animation.DROP,
+      icon: defaultIcon,
+      id: i
+    });
+
+    markers.push(marker);
+    marker.setMap(map);
+
+    marker.addListener('click', function() {
+      populateInfoWindow(this, largeInfowindow);
+    });
+
+    marker.addListener('mouseover', function() {
+      this.setIcon(highlightedIcon);
+    });
+    marker.addListener('mouseout', function() {
+      this.setIcon(defaultIcon);
+    });
+  } //for
+
+}
+*/
+
+
+
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 40.7413549, lng: -73.9980244},
@@ -11,16 +143,8 @@ function initMap() {
   var defaultIcon = makeMarkerIcon('0091ff');
   var highlightedIcon = makeMarkerIcon('FFFF24');
 
-  var locations = [
-    {title: 'Park Ave Penthouse', location: {lat: 40.7713024, lng: -73.9632393}},
-    {title: 'Chelsea Loft', location: {lat: 40.7444883, lng: -73.9949465}},
-    {title: 'Union Square Open Floor Plan', location: {lat: 40.7347062, lng: -73.9895759}},
-    {title: 'East Village Hip Studio', location: {lat: 40.7281777, lng: -73.984377}},
-    {title: 'TriBeCa Artsy Bachelor Pad', location: {lat: 40.7195264, lng: -74.0089934}},
-    {title: 'Chinatown Homey Space', location: {lat: 40.7180628, lng: -73.9961237}}
-  ];
-
   var largeInfowindow = new google.maps.InfoWindow();
+
   for(var i = 0; i < locations.length; i++) {
     var position = locations[i].location;
     var title = locations[i].title;
@@ -75,48 +199,3 @@ function populateInfoWindow(marker, infowindow) {
   }
 
 }
-
-/*
-function populateInfoWindow(marker, infowindow) {
-  // Check to make sure the infowindow is not already opened on this marker.
-  if (infowindow.marker != marker) {
-    // Clear the infowindow content to give the streetview time to load.
-    infowindow.setContent('');
-    infowindow.marker = marker;
-    // Make sure the marker property is cleared if the infowindow is closed.
-    infowindow.addListener('closeclick', function() {
-      infowindow.marker = null;
-    });
-    var streetViewService = new google.maps.StreetViewService();
-    var radius = 50;
-    // In case the status is OK, which means the pano was found, compute the
-    // position of the streetview image, then calculate the heading, then get a
-    // panorama from that and set the options
-    function getStreetView(data, status) {
-      if (status == google.maps.StreetViewStatus.OK) {
-        var nearStreetViewLocation = data.location.latLng;
-        var heading = google.maps.geometry.spherical.computeHeading(
-          nearStreetViewLocation, marker.position);
-          infowindow.setContent('<div>' + marker.title + '</div><div id="pano"></div>');
-          var panoramaOptions = {
-            position: nearStreetViewLocation,
-            pov: {
-              heading: heading,
-              pitch: 30
-            }
-          };
-        var panorama = new google.maps.StreetViewPanorama(
-          document.getElementById('pano'), panoramaOptions);
-      } else {
-        infowindow.setContent('<div>' + marker.title + '</div>' +
-          '<div>No Street View Found</div>');
-      }
-    }
-    // Use streetview service to get the closest streetview image within
-    // 50 meters of the markers position
-    streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView);
-    // Open the infowindow on the correct marker.
-    infowindow.open(map, marker);
-  }
-}
-*/
